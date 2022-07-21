8a2a527d9407019aa5212b33c1b4062569a831a6
Boren Xue <boren.xue@strakertranslations.com>
2022-07-22 10:56:31 +1200

    Store access token and OAuth state in the database

--- app/routers/slack.py
+++ app/routers/slack.py
@@ -3,22 +3,26 @@ from fastapi import APIRouter, Request
 from slack_bolt.async_app import AsyncApp
 from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
 from slack_bolt.oauth.async_oauth_settings import AsyncOAuthSettings
-from slack_sdk.oauth.installation_store.sqlalchemy import SQLAlchemyInstallationStore
-from slack_sdk.oauth.installation_store import FileInstallationStore
-from slack_sdk.oauth.state_store import FileOAuthStateStore
+from ..slack.installation_store import AsyncSQLAlchemyInstallationStore
+from ..slack.state_store import AsyncSQLAlchemyOAuthStateStore
 from ..database import engine
 
 
-installation_store = SQLAlchemyInstallationStore(
+installation_store = AsyncSQLAlchemyInstallationStore(
     client_id=os.getenv('SLACK_CLIENT_ID'),
     engine=engine,
     bots_table_name='slack_bots',
     installations_table_name='slack_installations',
 )
+state_store = AsyncSQLAlchemyOAuthStateStore(
+    expiration_seconds=1800,
+    engine=engine,
+    table_name='slack_oauth_states'
+)
 
 # Create the Slack tables if they do not exist.
-installation_store.installations.create(engine, checkfirst=True)
-installation_store.bots.create(engine, checkfirst=True)
+installation_store.create_tables()
+state_store.oauth_states.create(engine, checkfirst=True)
 
 oauth_settings = AsyncOAuthSettings(
     client_id=os.getenv('SLACK_CLIENT_ID'),
@@ -28,9 +32,8 @@ oauth_settings = AsyncOAuthSettings(
         'channels:history', 'groups:history',
         'im:history', 'mpim:history',
     ],
-    # TODO Use DB store
-    installation_store=FileInstallationStore(base_dir='./data/installations'),
-    state_store=FileOAuthStateStore(expiration_seconds=1800, base_dir='./data/states'),
+    installation_store=installation_store,
+    state_store=state_store,
     state_validation_enabled=True,
     install_page_rendering_enabled=False,
 )
