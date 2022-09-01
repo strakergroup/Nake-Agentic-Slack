d00314b5914b39fe4011963805f4a77aeec58653
Boren Xue <boren.xue@strakertranslations.com>
2022-09-01 18:15:23 +1200

    Allow different Watson Assistant environments

--- app/watson/assistant.py
+++ app/watson/assistant.py
@@ -1,12 +1,24 @@
-import os
+from pydantic import BaseSettings, Field
 from ibm_watson import AssistantV2
 
 from .response import WatsonResponse
 
 
-ASSISTANT_ID = os.getenv("WATSON_ASSISTANT_ID")
-if not ASSISTANT_ID:
-    raise AssertionError("The WATSON_ASSISTANT_ID environment variable is not set")
+class WatsonConfig(BaseSettings):
+    assistant_id: str = Field(env="WATSON_ASSISTANT_ID")
+    environment_id: str = Field(env="WATSON_ENVIRONMENT_ID")
+
+    class Config:
+        min_anystr_length = 1
+        allow_mutation = False
+        error_msg_templates = {
+            "value_error.missing": "The environment variable is missing",
+            "value_error.any_str.min_length": "The environment variable is missing",
+        }
+
+
+watson_config = WatsonConfig()
+
 
 # Automatically authenticated from the ibm-credentials.env file.
 # https://github.com/watson-developer-cloud/python-sdk#credential-file
@@ -14,8 +26,9 @@ assistant = AssistantV2(version="2021-11-27")
 
 
 def watson_message(text: str, user_id: str | None = None) -> WatsonResponse:
+    # TODO: rate limits?
     response = assistant.message_stateless(
-        ASSISTANT_ID,
+        watson_config.environment_id,
         input={"text": text},
         user_id=user_id,
     )
