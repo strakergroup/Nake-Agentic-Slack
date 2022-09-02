52045721a790bc6a65c4e34ffd6a9b228f53581d
Boren Xue <boren.xue@strakertranslations.com>
2022-09-02 16:59:49 +1200

    Remove requirement of connected account for some commands

--- app/slack/listeners.py
+++ app/slack/listeners.py
@@ -2,7 +2,6 @@
 commands, etc. from the Slack API.
 """
 
-from typing import Callable
 import re
 import json
 from pydantic import ValidationError
@@ -10,7 +9,7 @@ from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
 from slack_sdk.errors import SlackApiError
 
 from .app import app
-from .middleware import load_ray_client
+from .middleware import load_ray_client, require_ray_client
 from .logging import slack_log_decorator
 from .templates.models import NewJobForm, convert_pydantic_to_slack_error
 from .templates.messages import (
@@ -49,17 +48,6 @@ async def message_event(message, context, say, client):
         #     msg = NewJobMessage(context["channel_id"], message["ts"])
         #     await say(blocks=msg.blocks, text=msg.text)
         return
-
-    async def require_ray_client(callback: Callable[[None], None]):
-        if context["ray_client"]:
-            await callback()
-        else:
-            await client.chat_postEphemeral(
-                channel=context["channel_id"],
-                user=context["user_id"],
-                blocks=context["login_prompt"].blocks,
-                text=context["login_prompt"].text,
-            )
 
     async def show_job_status(job_id: str):
         response = await RayService.get_service(context["ray_client"]).get_job(job_id)
@@ -101,9 +89,8 @@ async def message_event(message, context, say, client):
         case "Job_Status":
             tj_number_entity = response.findEntity("tj-number")
             if tj_number_entity:
-                await require_ray_client(
-                    lambda: show_job_status(tj_number_entity.groups[0].upper())
-                )
+                if await require_ray_client(context):
+                    await show_job_status(tj_number_entity.groups[0].upper())
             else:
                 await say(JobStatusNoIdMessage().text)
         # case "New_Translation_Job":
@@ -116,9 +103,8 @@ async def message_event(message, context, say, client):
             tj_number_entity = response.findEntity("tj-number")
             if tj_number_entity:
                 # Show the job status if only a job id is entered.
-                await require_ray_client(
-                    lambda: show_job_status(tj_number_entity.groups[0].upper())
-                )
+                if await require_ray_client(context):
+                    await show_job_status(tj_number_entity.groups[0].upper())
             elif response.reply:
                 # Default to Watson Assistant fallback response if no other matches.
                 await say(response.reply)
@@ -180,51 +166,52 @@ async def ray_command(ack, say, respond, command, context, client):
         r"\s+", strip_formatting(command.get("text", "").strip().lower())
     )
     command_args = [strip_formatting(arg) for arg in command_args]
-    if context["ray_client"]:
-        match command_args:
-            case ["account"]:
+    match command_args:
+        case ["account"]:
+            if await require_ray_client(context):
                 await respond(WhoamiMessage(context["ray_client"].username).text)
-            case ["login" | "signin" | "connect"]:
-                await respond(
-                    blocks=context["login_prompt"].blocks,
-                    text=context["login_prompt"].text,
-                )
-            # case ["logout" | "signoff"]:
-            #     await respond("Logout prompt")
-            # case ["new"]:
-            #     files = await files_list_simple(client, count=110)
-            #     # Try to get the files from the last 3 messages to set as the
-            #     # default files to translate in the new job modal.
-            #     init_files = []
-            #     try:
-            #         response = await client.conversations_history(
-            #             channel=context["channel_id"],
-            #             limit=3,
-            #         )
-            #         for message in response["messages"]:
-            #             if message.get("files"):
-            #                 init_files = message.get("files")
-            #                 break
-            #     except SlackApiError:
-            #         pass
-            #     await client.views_open(
-            #         trigger_id=command["trigger_id"],
-            #         view=new_job_modal(
-            #             context["ray_client"].username,
-            #             file_options=files,
-            #             initial_files=init_files,
-            #         ),
-            #     )
-            case ["help" | ""]:
-                await respond(blocks=HelpMessage().blocks, text=HelpMessage().text)
-            case ["whatsnext"]:
-                await respond(
-                    blocks=WhatsNextMessage().blocks, text=WhatsNextMessage().text
-                )
-            case [command_text]:
-                # TODO strip text of markdown
-                match = re.fullmatch(r"tj\d+", command_text, re.IGNORECASE)
-                if match:
+        case ["login" | "signin" | "connect"]:
+            await respond(
+                blocks=context["login_prompt"].blocks,
+                text=context["login_prompt"].text,
+            )
+        # case ["logout" | "signoff"]:
+        #     await respond("Logout prompt")
+        # case ["new"]:
+        #     files = await files_list_simple(client, count=110)
+        #     # Try to get the files from the last 3 messages to set as the
+        #     # default files to translate in the new job modal.
+        #     init_files = []
+        #     try:
+        #         response = await client.conversations_history(
+        #             channel=context["channel_id"],
+        #             limit=3,
+        #         )
+        #         for message in response["messages"]:
+        #             if message.get("files"):
+        #                 init_files = message.get("files")
+        #                 break
+        #     except SlackApiError:
+        #         pass
+        #     await client.views_open(
+        #         trigger_id=command["trigger_id"],
+        #         view=new_job_modal(
+        #             context["ray_client"].username,
+        #             file_options=files,
+        #             initial_files=init_files,
+        #         ),
+        #     )
+        case ["help" | ""]:
+            await respond(blocks=HelpMessage().blocks, text=HelpMessage().text)
+        case ["whatsnext"] | ["whats", "next"]:
+            await respond(
+                blocks=WhatsNextMessage().blocks, text=WhatsNextMessage().text
+            )
+        case [command_text]:
+            match = re.fullmatch(r"tj\d+", command_text, re.IGNORECASE)
+            if match:
+                if await require_ray_client(context):
+                    # TODO: put ray_service in middleware.
                     response = await RayService.get_service(
                         context["ray_client"]
                     ).get_job(command_text)
@@ -243,16 +230,10 @@ async def ray_command(ack, say, respond, command, context, client):
                             headers=dict(response.response.headers.items()),
                             version="v3",
                         )
-                else:
-                    await respond(text=InvalidCommandMessage().text)
-            case _:
+            else:
                 await respond(text=InvalidCommandMessage().text)
-    else:
-        # Prompt login if accounts are not connected yet.
-        await respond(
-            blocks=context["login_prompt"].blocks,
-            text=context["login_prompt"].text,
-        )
+        case _:
+            await respond(text=InvalidCommandMessage().text)
 
 
 # @app.block_action("new_job", middleware=[load_ray_client])
--- app/slack/middleware.py
+++ app/slack/middleware.py
@@ -3,12 +3,15 @@
 See https://slack.dev/bolt-python/concepts#listener-middleware.
 """
 
+import logging
+from sentry_sdk import capture_message
+from slack_bolt.context.async_context import AsyncBoltContext
 from ray_logger.slack import SlackAppLog
 
 from .app import app
 from .logging import init_slack_app_log
-from .templates.messages import LoginMessage
-from ..auth.connector import get_ray_client, get_app_id
+from .templates.messages import SlackMessage, LoginMessage
+from ..auth.connector import RayClient, get_ray_client, get_app_id
 
 
 # -----------------------------------------------------------------------------
@@ -61,3 +64,40 @@ async def load_ray_client(context, body, next) -> None:
         context["log"].slack_log.client_id = context["ray_client"].id
 
     await next()
+
+
+# -----------------------------------------------------------------------------
+# Middleware helper functions
+# -----------------------------------------------------------------------------
+
+
+async def require_ray_client(context: AsyncBoltContext) -> bool:
+    """Checks if a Slack user is connected to a DeltaRAY account by checking
+    the context. If not connected, then post a message prompting the user
+    to connect their account. (Requires the `load_ray_client` middleware.)
+
+    Returns:
+        bool: The Slack user has a connected DeltaRAY account.
+    """
+    if isinstance(context.get("ray_client"), RayClient):
+        return True
+
+    if not isinstance(login_message := context.get("login_prompt"), SlackMessage):
+        logging.warning('"login_prompt" is not in the context')
+        capture_message('Slack: "login_prompt" is not in the context', "warning")
+        return False
+
+    # Send login prompt if no DeltaRAY account is connected.
+    if context.respond.response_url:
+        await context.respond(
+            text=login_message.text,
+            blocks=login_message.blocks,
+        )
+    else:
+        await context.client.chat_postEphemeral(
+            channel=context.get("channel_id") or context.get("user_id"),
+            user=context.get("user_id"),
+            text=login_message.text,
+            blocks=login_message.blocks,
+        )
+    return False
