e812122db22085bcc65c8a99894e87527f1e2894
Boren Xue <boren.xue@strakertranslations.com>
2022-11-14 12:42:11 +1300

    Integrate Elastic APM

--- app/config.py
+++ app/config.py
@@ -24,6 +24,7 @@ class StrakerConfig(BaseSettings):
     # Settings from environment variables.
     environment: Environment = Field(env="ENVIRONMENT")
     sentry_dsn: str | None = Field(None, env="SENTRY_DSN")
+    elastic_apm_server_url: str | None = Field(None, env="ELASTIC_APM_SERVER_URL")
     # Derived settings.
     base_url: HttpUrl = None
     slack_deltaray_key: SecretBytes = None
--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ import sentry_sdk
 from sentry_sdk.integrations.starlette import StarletteIntegration
 from sentry_sdk.integrations.fastapi import FastApiIntegration
 from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
+from elasticapm.contrib.starlette import make_apm_client, ElasticAPM
 
 from .config import config, Environment
 from .routers import slack, ray, health
 
 
+# Configure Sentry
 if not config.sentry_dsn:
     logging.warning("Sentry is not set up (SENTRY_DSN is missing)")
 sentry_sdk.init(
@@ -27,11 +29,25 @@ sentry_sdk.init(
 )
 
 
+# Configure Elastic APM
+apm = make_apm_client(
+    {
+        "SERVICE_NAME": "slack-ray-translator",
+        "SERVER_URL": config.elastic_apm_server_url,
+        "ENVIRONMENT": config.environment.value
+        if config.environment != Environment.live
+        else "production",
+    }
+)
+
+
 # Configure FastAPI
 app = FastAPI()
 app.include_router(slack.router)
 app.include_router(ray.router)
 app.include_router(health.router)
+if config.elastic_apm_server_url:
+    app.add_middleware(ElasticAPM, client=apm)
 
 
 @app.get("/")
