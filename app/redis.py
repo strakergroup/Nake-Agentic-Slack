a06980c48f759213b095c01e0eab57ac639db5d6
Boren Xue <boren.xue@strakertranslations.com>
2022-11-30 13:17:13 +1300

    Update straker-utils package

--- app/redis.py
+++ app/redis.py
@@ -1,4 +1,4 @@
-from straker_utils.redis.asyncio import get_redis_sentinel
+from straker_utils.redis.asyncio import get_redis_auto
 
 
-redis_conn = get_redis_sentinel()
+redis_conn = get_redis_auto()
