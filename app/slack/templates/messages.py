a51faa61966a6df3956b6fe27b4e2df10d5463a5
Boren Xue <boren.xue@strakertranslations.com>
2023-06-06 13:09:18 +1200

    RAY-59799 Update onboarding message

--- app/slack/templates/messages.py
+++ app/slack/templates/messages.py
@@ -58,13 +58,13 @@ class OnboardingMessage(SlackMessage):
         self, user_id: str, team_id: str, app_id: str, channel_id: str
     ) -> None:
         super().__init__(
-            "The Straker App has been sucessfully installed in your Slack workspace! :tada:",
+            "Welcome to the Straker App for Slack! :tada:",
             [
                 {
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": "The Straker App has been sucessfully installed in your Slack workspace! :tada:",
+                        "text": "Welcome to the Straker App for Slack! :tada:",
                     },
                 },
                 {
