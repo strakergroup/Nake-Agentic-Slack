30a1838ddcf29382fac7f4a39f57478410ba8be0
Wade Norman <wade.norman@strakertranslations.com>
2023-09-29 11:30:03 +1300

    RAY-62538 update to insert user details pre login

--- app/auth/connector.py
+++ app/auth/connector.py
@@ -732,23 +732,23 @@ async def approve_pending_groups(
 
 # log new user info
 async def log_new_user_info(user):
-    with engines["ray_integration_log"].connect() as conn:
+    with engines["ray_integration"].connect() as conn:
         sql = text(
             """
-            INSERT IGNORE INTO slack_user_log
-                (slack_user_id, slack_team_id, slack_enterprise_id, slack_enterprise_name, tz, tz_label, email, name)
+            INSERT IGNORE INTO slack_user_details
+                (slack_user_id, slack_team_id, slack_enterprise_id, slack_enterprise_name, timezone, timezone_label, client_email, client_full_name)
             VALUES
-                (:user_id, :team_id, :enterprise_id, :enterprise_name, :tz, :tz_label, :email, :name)
+                (:user_id, :team_id, :enterprise_id, :enterprise_name, :timezone, :timezone_label, :client_email, :client_full_name)
             """
         ).bindparams(
             user_id=user.get("id", ""),
             team_id=user.get("team_id", ""),
             enterprise_id=user.get("enterprise_user", {}).get("enterprise_id", ""),
             enterprise_name=user.get("enterprise_user", {}).get("enterprise_name", ""),
-            tz=user.get("tz", ""),
-            tz_label=user.get("tz_label", ""),
-            email=user.get("profile", {}).get("email", ""),
-            name=user.get("profile", {}).get("real_name_normalized", ""),
+            timezone=user.get("tz", ""),
+            timezone_label=user.get("tz_label", ""),
+            client_email=user.get("profile", {}).get("email", ""),
+            client_full_name=user.get("profile", {}).get("real_name_normalized", ""),
         )
         conn.execute(sql)
         conn.commit()
