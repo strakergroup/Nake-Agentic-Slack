99b1241effd471204133ae44efae4b35b33ccf26
Eric Zhang <eric.zhang@strakertranslations.com>
2023-10-24 11:59:07 +1300

    Fix and UI listing issue

--- app/slack/templates/messages.py
+++ app/slack/templates/messages.py
@@ -831,8 +831,7 @@ class JobListMessage(SlackMessage):
                     }
                 )
                 if job.status != "COMPLETED" and job.batches != "[]":
-                    jobs_blocks.insert(
-                        3,
+                    jobs_blocks.append(
                         {
                             "type": "actions",
                             "elements": [
