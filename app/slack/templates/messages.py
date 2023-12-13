f9d6afb268b3ac765a201ee71cf07f2184289c62
Ashbee Morgado <ashbee.morgado@strakertranslations.com>
2023-12-13 09:51:48 +0800

    fix bug

--- app/slack/templates/messages.py
+++ app/slack/templates/messages.py
@@ -562,32 +562,6 @@ class JobStatusMessage(SlackMessage):
                     ],
                 },
             )
-        {% comment %} if job.status == "COMPLETED":
-            job_status_block.insert(
-                3,
-                {
-                    "type": "actions",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Show Completed Files",
-                                "emoji": True,
-                            },
-                            "action_id": "file_list_1",
-                            "value": json.dumps(
-                                {
-                                    "id": job.id,
-                                    "page": 1,
-                                    "page_size": 5,
-                                    "replace_original": False,
-                                }
-                            ),
-                        }
-                    ],
-                },
-            ) {% endcomment %}
         if job_prediction != "":
             job_status_block.insert(
                 1,
