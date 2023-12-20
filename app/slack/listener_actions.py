c42b8187eded45bc4c09773b8b096994376c20a2
Gautam Shrestha <gautam.shrestha@strakertranslations.com>
2023-12-20 20:01:04 +1300

    RAY-63217 Removing the debugging code block

--- app/slack/listener_actions.py
+++ app/slack/listener_actions.py
@@ -916,11 +916,7 @@ async def post_file_list(
     )
     try:
         if job is not None:
-            print("------------------------")
-            print(job)
             msg = FileListMessage(job, ray_client.id)
-            print("------------------------")
-            print(msg.blocks)
             if context.response_url:
                 return await context.respond(
                     text=msg.text, blocks=msg.blocks, replace_original=replace_original
