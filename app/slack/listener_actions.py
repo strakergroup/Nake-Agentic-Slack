5e3efd0a143a1972e6e975eedd9bb0535d7d4310
Wade Norman <wade.norman@strakertranslations.com>
2024-01-17 10:58:02 +1300

    RAY-64658 slack upload file add channel id to new job file option action

--- app/slack/listener_actions.py
+++ app/slack/listener_actions.py
@@ -732,6 +732,7 @@ async def post_insights(
 
 async def show_quote_form_modal(
     context: AsyncBoltContext,
+    channel_id: str,
     trigger_id: str,
     ray_client: RayClient,
     *,
@@ -778,6 +779,7 @@ async def show_quote_form_modal(
         trigger_id=trigger_id,
         view=new_job_modal(
             ray_client.username,
+            channel_id=channel_id,
             file_options=files,
             initial_files=initial_files,
         ),
