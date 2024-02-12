894e47c75b0ea5870ca3724a7d398fff870ccaa4
Gautam Shrestha <gautam.shrestha@strakertranslations.com>
2024-02-13 08:55:30 +1300

    RAY-65139 Removed the Straker workspace for Direct Login

--- app/slack/templates/blocks.py
+++ app/slack/templates/blocks.py
@@ -78,7 +78,7 @@ def home_auth_blocks(
     if enterprise_id:
         # enterprise_id == "E04RDMG8XP1" is for UAT
         # enterprise_id == "EUJJ37YFR" is for Live IBM Translate
-        if (enterprise_id == "EUJJ37YFR" or enterprise_id == "E04RDMG8XP1"):
+        if enterprise_id == "EUJJ37YFR":
             msg[1]["elements"].append(
                 {
                     "type": "button",
