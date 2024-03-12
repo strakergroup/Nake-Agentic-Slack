b68f6aa21d57066905747022822b70ed9be878a5
Boren Xue <boren.xue@strakertranslations.com>
2024-03-13 10:33:06 +1300

    Fix base_url config required and add domain env vars

--- app/config.py
+++ app/config.py
@@ -42,7 +42,7 @@ class StrakerConfig(BaseSettings):
     # taus_api_key: SecretStr = Field(min_length=1)
     elastic_apm_server_url: str | None = None
     # Derived settings.
-    base_url: HttpUrl
+    base_url: HttpUrl = None
     buglog_listener_url: str = ""
     slack_deltaray_key: SecretBytes = SecretBytes(b"")
     slack_queue_proxy_secret: SecretStr = SecretStr("")
