8250053db14e27435bdc8ff19740f65ab16646e1
Abhi Sharma <abhi.sharma@strakergroup.com>
2024-06-19 13:51:19 +1200

    RAY-66678 fix translation

--- app/slack/templates/views.py
+++ app/slack/templates/views.py
@@ -217,9 +217,7 @@ async def home_view(
                         "text": {
                             "type": "plain_text",
                             "emoji": True,
-                            "text": _(
-                                ":speech_balloon: Translation settings"
-                            ),
+                            "text": ":speech_balloon: " + _("Translation settings"),
                         },
                         "value": json.dumps(
                             {
