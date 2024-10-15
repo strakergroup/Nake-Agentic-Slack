d84cacf278faf4b21503e451455203c2134477cf
Gautam Shrestha <gautam.shrestha@strakertranslations.com>
2024-10-16 09:59:10 +1300

    RAY-69897 Fixing length for files if initial file is set

--- app/slack/listener_actions.py
+++ app/slack/listener_actions.py
@@ -1120,6 +1120,9 @@ async def show_quote_form_modal(
     """
     # Include a bit more than the max 100 options due to hidden files.
     files = await get_file_options_cached(context["channel_id"])
+    # Reduce list to 20 if initial files are set.
+    if initial_files and len(files)+len(initial_files) > 20:
+        files = files[:20-len(initial_files)]
     # Set initial selected files.
     if not initial_files and check_last_messages > 0:
         # Check last 100 messages maximum.
