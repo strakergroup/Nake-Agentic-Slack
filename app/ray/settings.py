6556ec7120233f45cbc5162dd5f1868c23939d04
Wade Norman <wade.norman@strakertranslations.com>
2024-11-18 09:59:04 +1300

    RAY-70251 fix team_id not passed when fetching settings

--- app/ray/settings.py
+++ app/ray/settings.py
@@ -365,7 +365,7 @@ def get_auto_translate_settings_and_langs(
     team_id = team_id or context.team_id
     with Session(engines["ray_integration"]) as session:
         channel_settings = get_or_create_auto_translate_group_settings(
-            session, context, channel_id, context.team_id
+            session, context, channel_id, team_id
         )
         results = session.scalars(
             select(SlackGroupSettingsTranslationLangs.lang).where(
