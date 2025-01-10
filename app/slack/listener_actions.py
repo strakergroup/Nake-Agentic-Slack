6adf28f98d8f719ca9c7392115116cefa7714174
Wade Norman <wade.norman@strakertranslations.com>
2025-01-10 16:43:16 +1300

    Revert "hotfix avoid media probe"
    This reverts commit 8b4e6425a698cd82abe30ab1123adf053dbcbc06.

--- app/slack/listener_actions.py
+++ app/slack/listener_actions.py
@@ -65,7 +65,7 @@ from ..ray.service import RayService, get_job_predictions
 from ..ray.settings import (
     get_auto_translate_settings_and_langs,
 )
-from ..ray.utils import is_ibm_enterprise
+from ..ray.utils import get_media_duration, is_ibm_enterprise
 from ..watson import watson_message
 from .select_options import get_file_options_cached
 
@@ -101,7 +101,7 @@ async def respond_to_message(
                     # duration_ms = file_info["file"].get("duration_ms", 0)
                     duration_ms = 0
                     if not duration_ms:
-                        duration_ms = 1
+                        duration_ms = get_media_duration(download_url, client.token)
                     file_name = file_info["file"]["name"]
                     token = client.token
                     # send video to wb consumer
