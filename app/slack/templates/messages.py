f8399d747172236302930d60d1d7655d3647c342
Justin Cole <justin.cole@strakergroup.com>
2025-02-24 11:10:05 +1300

    resolve merge conflicts

--- app/slack/templates/messages.py
+++ app/slack/templates/messages.py
@@ -1731,7 +1731,7 @@ class NewJobMessage(SlackMessage):
                     "text": {
                         "type": "mrkdwn",
                         "text": _(
-                            "*• Quality Evaluation* - AI translate your content and receive quality translation scores, then Verify with Straker to send for human verification"
+                            "*• Quality Evaluation* - AI translate your content and receive translation quality scores, then verify with Straker to send for human verification"
                         ),
                     },
                     "accessory": {
