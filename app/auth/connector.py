9c63e22e1a920c0eaf84c9331aa083e7ac37f5de
Suresh Murugesan <suresh.murugesan@strakertranslations.com>
2025-03-21 13:04:36 +1300

    RAY-72149 Updating product column for Direct login IBM users

--- app/auth/connector.py
+++ app/auth/connector.py
@@ -1232,7 +1232,7 @@ def create_client_and_mglink(
         sqlMem = text(
             """
             INSERT INTO obj_m_member
-                (obj_uuid, email_primary, login, password, password_updated, given_name, family_name, created, modified, account_manager, groupid, subscribed, active, email_active)
+                (obj_uuid, email_primary, login, password, password_updated, given_name, family_name, created, modified, account_manager, groupid, product, subscribed, active, email_active)
             VALUES
                 (:obj_uuid, :email_primary, :email_primary, :password, now(), :given_name, :family_name, now(), now(), :account_manager, :groupid, 1, 1, 1)
             """
@@ -1244,6 +1244,7 @@ def create_client_and_mglink(
             password=hash_object.hexdigest().upper(),
             account_manager=account_manager,
             groupid=group_id,
+            product="Slack",
         )
         conn.execute(sqlMem)
         conn.commit()
