9f09bea276454c8932f06fadd123df3eb91152f8
Justin Cole <justin.cole@strakergroup.com>
2025-05-23 14:30:22 +1200

    comment out source language detection

--- app/slack/listener_actions.py
+++ app/slack/listener_actions.py
@@ -379,17 +379,22 @@ async def auto_translate_message(
     assert context.channel_id  # TODO enforce this
     # TODO make this fetch all settings for channel
     settings = get_auto_translate_settings_and_langs(context, context.channel_id)
-    detected_source_lang_response = await detect_language(context["ray"].client, text)
+    target_langs = [langs["target_lang"] for langs in settings]
+    # detected_source_lang_response = await detect_language(context["ray"].client, text)
 
-    # Remove the detected source language from the target languages
-    target_langs = [
-        langs["target_lang"]
-        for langs in settings
-        if langs["target_lang"] != detected_source_lang_response.language
-    ]
+    # # Remove the detected source language from the target languages
+    # target_langs = [
+    #     langs["target_lang"]
+    #     for langs in settings
+    #     if langs["target_lang"] != detected_source_lang_response.language
+    # ]
 
-    if not target_langs or not settings:
+    # if not target_langs or not settings:
+    #     return
+
+    if not settings:
         return
+
     try:
         source_lang, translations = await get_ai_translation(
             context, text, target_langs, "channel_translation"
