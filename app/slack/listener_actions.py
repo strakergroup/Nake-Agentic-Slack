44df6d081f24a31fa32d5a83730bdac97ab02df7
Wade Norman <wade.norman@strakertranslations.com>
2025-07-28 15:19:37 +1200

    RAY-74268 update the message for human translation

--- app/slack/listener_actions.py
+++ app/slack/listener_actions.py
@@ -1953,7 +1953,7 @@ async def submit_verification_job(
 
         # Send initial confirmation
         msg = _(
-            "You've successfully submitted your document(s) for quality evaluation. Click on the button below to view the overall scores and proceed further to send for human translation"
+            "Thank you for sending your document for human translation! We will notify you as soon as the translation is complete."
         )
         response = await client.chat_postMessage(
             channel=user_id,
