9927bbcd852eb7ef7fb3ce1041152587568a75e5
Justin Cole <justin.cole@strakergroup.com>
2025-10-06 15:39:28 +1300

    added file complexity error

--- app/ray/events/models.py
+++ app/ray/events/models.py
@@ -141,6 +141,7 @@ class MtErrorTypes(str, Enum):
     INSUFFICIENT_BALANCE = "insufficient_balance"
     SAMPLE_TEXT_NOT_FOUND = "sample_text_not_found"
     CONVERSION_ERROR = "conversion_error"
+    FILE_COMPLEXITY_ERROR = "file_complexity_error"
     OTHER = "other"
 
 
